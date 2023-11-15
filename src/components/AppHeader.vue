4bf242cd3741d6f13e75f5c3e354443605aaf0c2
Marta Scardillo <martascardillo02@gmail.com>
2023-11-15 11:45:06 +0100

    Add HeaderInfo file

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script>
+import HeaderInfo from './HeaderComponents/HeaderInfo.vue';
+
+export default {
+    data() {
+        return {};
+    },
+
+    components: {
+        HeaderInfo,
+    },
+};
+</script>
+
+<template>
+    <HeaderInfo />
+</template>
+
+<style></style>
