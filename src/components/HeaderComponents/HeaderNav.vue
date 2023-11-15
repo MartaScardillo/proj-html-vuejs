0546db25432e332f1f0b8ece351f59e915d43348
Marta Scardillo <martascardillo02@gmail.com>
2023-11-15 17:14:24 +0100

    Add HeaderNav

--- src/components/HeaderComponents/HeaderNav.vue
+++ src/components/HeaderComponents/HeaderNav.vue
@@ -1,11 +1,59 @@
 <script>
 export default {
     data() {
-        return {};
+        return {
+            logo: 'logo.png',
+            tabs: [
+                'Home',
+                'About',
+                'Services',
+                'Traning',
+                'Blogs',
+                'Get In Touch!',
+            ],
+        };
     },
 };
 </script>
 
-<template>a</template>
+<template>
+    <div class="container">
+        <ul class="d-flex align-items-center justify-content-between">
+            <li><img :src="logo" alt="" /></li>
+            <li>
+                <ul class="d-flex align-items-center fs-5">
+                    <li class="list_item p-4" v-for="(tab, index) in tabs">
+                        <span class="orange" v-if="index == 0">{{ tab }}</span>
+                        <span
+                            class="blue_hover px-4 py-3 text-white background_orange rounded-pill"
+                            v-else-if="index == 5"
+                            >{{ tab }}</span
+                        >
+                        <span class="orange_hover light-gray" v-else>{{
+                            tab
+                        }}</span>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</template>
 
-<style></style>
+<style lang="scss">
+@use '../style/partials/common' as *;
+@use '../style/partials/variables' as *;
+
+.list_item {
+    .blue_hover {
+        &:hover {
+            background-color: $blue;
+        }
+    }
+
+    .orange_hover {
+        &:hover {
+            color: $orange;
+        }
+    }
+}
+</style>
